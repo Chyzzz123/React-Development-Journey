1be4c970f4f079035f58a83807b5de9cb59ba17b
Chyyan Cacnio <chyyancacnio@gmail.com>
2025-11-06 20:08:58 +0800

    Day 1 with better practice with components and props

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -2,13 +2,10 @@ import Day1Component from "./components/Day1Component";
 function App() { // the "function App()" defines a functional components
 
   return ( // the "return" statement defines what the component will rended
-    <div className="main-app-container">
-      <h1>My React Development Journey: Day 1 Practice</h1>
-      
-      <Day1Component name="Chyyan" num1={5} num2={3} />
-      
-      {/* Day 2 work will go below */}
-      {/* <Day2Component /> */}
+    <div>
+      <Day1Component name="Chyyan" age={21} />
+      <Day1Component name="Edward" age={18} />
+      <Day1Component name="Cacnio" age={32} />
     </div>
   )
 }
--- routing/src/components/Day1Component.jsx
+++ routing/src/components/Day1Component.jsx
@@ -1,16 +1,14 @@
+import React from "react"
 
-function Day1Component() { // the "Day1Component ()" defines a functional components
+function Day1Component(props) {
 
-  const name = "Chyyan";
-  const x = 5;
-  const y = 3;
 
-  return ( // the "return" statement defines what the component will rended
+  return (
     <div>
-      <h1>Hello {name}</h1>
-      <h1>the sum is {x + y}</h1>
+      <h3>Name: {props.name}</h3>
+      <h3>Age: {props.age}</h3>
     </div>
   )
 }
 
-export default Day1Component // the "export default App" allows this component to be imported elsewhere
+export default Day1Component
